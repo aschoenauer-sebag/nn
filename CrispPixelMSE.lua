6d59710bfa6c06bbe09e7275b0ec0c9e0da6a23c
lalil0u <a.lalil0u@gmail.com>
2018-01-10 11:18:19 +0100

    Adding CrispPixelMSE

---
+++ CrispPixelMSE.lua
@@ -0,0 +1,45 @@
+-- PIXEL MSE CRITERION
+-- Created by IntelliJ IDEA.
+-- User: asebag
+-- Date: 12/27/17
+-- Time: 4:23 PM
+-- To change this template use File | Settings | File Templates.
+--
+--Checked that the gradient value is approx. equivalent to the differences finies using test.lua
+----> it works
+
+local CrispPixelMSE, parent = torch.class('nn.CrispPixelMSE', 'nn.Criterion')
+
+function CrispPixelMSE:__init(power, epsilon)
+    parent.__init(self)
+
+    self.power = power or 2
+    self.eps = epsilon or 1e-6
+end
+
+function CrispPixelMSE:multiplicator(target)
+
+    return torch.add(
+                        torch.add(target, self.eps):pow(-self.alpha),
+                        torch.add(-target, 1+self.eps):pow(-self.alpha)
+                     )
+end
+
+function CrispPixelMSE:updateOutput(input,target)
+
+    self.output = (input-target):pow(2)
+    self.output = (self.output:cmul(self:multiplicator(target))
+                                            ):sum()
+
+    self.output = self.output/input:nElement()
+
+    return self.output
+end
+
+function CrispPixelMSE:updateGradInput(input, target)
+    self.gradInput = (input-target):cmul(self:multiplicator(target)):mul(2/input:nElement())
+
+   return self.gradInput
+end
+
+
